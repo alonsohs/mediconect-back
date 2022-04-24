52f9646099df7ec72772c884920d3e3619095d38
alonsohs <alonsohdez14@gmail.com>
2022-04-23 21:51:02 -0500

    Add index html file for /

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
+const path = require('path')
 const { checkApiKey } = require('./middlewares/auth.handler');
 
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
@@ -19,7 +20,7 @@ app.use(cors(options));
 require('./utils/auth');
 
 app.get('/', (req, res) => {
-  res.send('Mediconect Express server');
+  res.sendFile(path.join(__dirname, '/index.html'))
 });
 
 routerApi(app);
